340a9a9eb3d1edda74f2e720257e1adcb4834337
vps4618 <vishwapraveen4618@gmail.com>
2022-08-27 02:09:21 +0530

    created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { ItemDetailsComponent } from './components/item-details/item-details.component';
+import { SearchItemsComponent } from './components/search-items/search-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemDetailsComponent,
+    SearchItemsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
